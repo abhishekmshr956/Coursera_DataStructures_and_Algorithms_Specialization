873edf326667fbbf9d2113e0f48c457a2df013c0
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-08-05 23:54:09 -0700

    merge sort

--- IIT_Madras_PDS/week2_AnalysisofAlgorithms/sort.py
+++ IIT_Madras_PDS/week2_AnalysisofAlgorithms/sort.py
@@ -10,3 +10,67 @@ def SelectionSort(l):
                 min_ix = j
         (l[i], l[min_ix]) = (l[min_ix], l[i])
     return l
+
+# O(n^2)
+def InsertionSort(L):
+    n = len(L)
+    if n < 1:
+        return L
+
+    for i in range(n):
+        j = i
+        while(j>0 and L[j] < L[j-1]):
+            (L[j], L[j-1]) = (L[j-1], L[j])
+            j = j-1
+    return L
+
+def Insert(L, v):
+    n = len(L)
+    if n == 0:
+        return [v]
+    if v >= L[-1]:
+        return L+[v]
+    else:
+        return Insert(L[:-1], v)+L[-1:]
+    
+def ISort(L):
+    n = len(L)
+    if n < 1:
+        return L
+    L = Insert(ISort(L[:-1]), L[-1])
+    return L
+
+
+## Merge sort
+# O(n logn)
+def merge(A, B):
+    m, n = len(A), len(B)
+    C, i, j, k = [], 0, 0, 0
+    while k < m+n:
+        if i == m:
+            C.extend(B[j:])
+            k = k + n-j
+        elif j == n:
+            C.extend(A[i:])
+            k = k + m-i
+        elif A[i] < B[j]:
+            C.append(A[i])
+            i, k = i+1, k+1
+        else:
+            C.append(B[j])
+            j, k = j+1, k+1
+    return C
+
+
+def mergesort(A):
+    n = len(A)
+
+    if n <= 1:
+        return A
+    
+    L = mergesort(A[:n//2])
+    R = mergesort(A[n//2:])
+    
+    B = merge(L, R)
+
+    return B
