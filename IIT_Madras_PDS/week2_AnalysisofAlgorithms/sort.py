c975eb0a1ce2b905e7b364f0b41616ab9d04bae9
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-08-06 10:41:14 -0700

    quick sort

--- IIT_Madras_PDS/week2_AnalysisofAlgorithms/sort.py
+++ IIT_Madras_PDS/week2_AnalysisofAlgorithms/sort.py
@@ -74,3 +74,21 @@ def mergesort(A):
     B = merge(L, R)
 
     return B
+
+# Quick sort
+# average run time of O(nlog n)
+def quicksort(L, l, r): # Sort L[l:r]
+    if r - l <= 1:
+        return L
+    pivot, lower, upper = L[l], l+1, l+1
+    for i in range(l+1, r):
+        if L[i] > pivot:
+            upper += 1
+        else:
+            L[i], L[lower] = L[lower], L[i]
+            lower, upper = lower+1, upper+1
+    L[l], L[lower-1] = L[lower-1], L[l]
+    lower = lower - 1
+    quicksort(L, l, lower)
+    quicksort(L, lower+1, upper)
+    return L
