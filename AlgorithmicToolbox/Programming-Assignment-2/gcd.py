eb8f3eeca2654391a2f04bb68ef989ade5754542
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-08-22 14:37:29 -0700

    gcd fast implemented

--- AlgorithmicToolbox/Programming-Assignment-2/gcd.py
+++ AlgorithmicToolbox/Programming-Assignment-2/gcd.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def gcd_naive(a, b):
     gcd = 1
     for i in range(1, min(a, b) + 1):
@@ -5,12 +7,31 @@ def gcd_naive(a, b):
             gcd = i
     return gcd
 
-def custom_test():
+def gcd(a, b):
+    a, b = min(a,b), max(a,b)
+    if b % a == 0:
+        return a
+    else:
+        return gcd(a, b % a)
+    
+def stress_test(N, M):
+    while True:
+        p, q = np.random.randint(1, M, N)
+        result1 = gcd_naive(p, q)
+        result2 = gcd(p, q)
+        if result1 == result2:
+            print("Ok")
+        else:
+            print(f"Wrong answer: gcd for {p, q}: naive: {result1}, fast: {result2}")
+            return
+
+
+def custom_test(f):
     test_cases = [(128, 96), (22, 26), (360, 210), (108, 144), (2, 24), (1,5), (6,14)]
     test_results = [32, 2, 30, 36, 2, 1, 2]
     for i in range(len(test_cases)):
         p, q = test_cases[i]
-        result = gcd_naive(p, q)
+        result = f(p, q)
         if result == test_results[i]:
             print(f"OK: gcd of {test_cases[i]} = {result}")
         else:
@@ -19,4 +40,5 @@ def custom_test():
 if __name__ == '__main__':
     # a, b = map(int, input().split())
     # print(gcd_naive(a, b))
-    custom_test()
+    # custom_test(gcd)
+    stress_test(2, 100000)
