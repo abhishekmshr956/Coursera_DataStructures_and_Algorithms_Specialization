b8688dffb5722350b1ec5551eb59c923309a30f0
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-08-21 21:29:25 -0700

    stress test

--- AlgorithmicToolbox/Programming-Assignment-1/max-pairwise-product.py
+++ AlgorithmicToolbox/Programming-Assignment-1/max-pairwise-product.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def max_pairwise_product_naive(numbers):
     n = len(numbers)
     max_product = 0
@@ -16,7 +18,22 @@ def max_pairwise_product(numbers):
     m2 = max(numbers)
     return m1 * m2
 
+def stress_test(N, M):
+    while True:
+        n = np.random.randint(2, N)
+        A = np.random.randint(0, M, n).tolist()
+        print(A)
+        result1 = max_pairwise_product_naive(A)
+        result2 = max_pairwise_product(A)
+        if result1 == result2:
+            print("OK")
+        else:
+            print(f"Wrong answer: {result1}, {result2}")
+            return
+
 if __name__ == '__main__':
-    _ = int(input())
-    input_numbers = list(map(int, input().split()))
-    print(max_pairwise_product(input_numbers))
+    # _ = int(input())
+    # input_numbers = list(map(int, input().split()))
+    N, M = list(map(int, input().split()))
+    stress_test(N, M)
+    # print(max_pairwise_product(input_numbers))
